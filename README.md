91de2e18c4f99dd5ebd244bc4e8bad0ac8929faf
Kishlaya Jaiswal <kishlaya.j@gmail.com>
2017-06-07 17:58:08 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Long Tweeetr
+
+Web app to help you post long tweets directly.
+
+Powered by Express, a Node.js framework, using Twitter REST API.
+
+## Usage
+
+First, you'll have to create your twitter app and get the 'Consumer Key', 'Consumer Secret' for the app. Set them as environment variables as follows: 'CONSUMER_KEY=&lt;your consumer key&gt;' and 'CONSUMER_SECRET=&lt;your consumer secret&gt;'.
+
+1. 'git clone https://github.com/kishlaya/long-tweeetr.git'
+2. 'cd long-tweeetr'
+3. 'npm install'
+4. 'npm start'
+5. Go to browser and load http://localhost:<port> and voila!
+
+## Example
+
+You can find already deployed instance of app [here](https://long-tweeetr.herokuapp.com)
---
+++ index.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var app = express();
+var twitter = require('./lib/twitter-control.js')(app);
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static('static'));
+app.set('view engine', 'pug');
+
+app.get('/', function(req, res) {
+	res.render('index');
+});
+
+app.get('/tweet', function(req, res) {
+	if(!authenticate()) {
+		res.status(500);
+		res.render('error', {error: "Authentication problem! Please sign-in again."});
+	}
+	else {
+		username = req.query.name;
+		screenName = req.query.screen_name;
+		res.render('tweet', {user: username, screenName: screenName});
+	}
+});
+
+app.get('/success', function(req, res) {
+	if(!authenticate()) {
+		res.status(500);
+		res.render('error', {error: "Authentication problem! Please sign-in again."});
+	}
+	else {
+		res.render('success');
+	}
+});
+
+app.use(function (err, req, res, next) {
+	res.status(500);
+	res.render('error');
+	console.log(err);
+});
+
+app.listen(5000, function() {
+	console.log("Listening on port " + app.get('port'));
+});
